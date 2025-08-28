b093fe68fe31514bd8b9f39390fbcf9944d2c527
FlepsGit <felipe.cp.fecap@gmail.com>
2025-08-28 18:14:08 -0300

    2º commit

--- fecart_prototipo.py
+++ fecart_prototipo.py
@@ -3,19 +3,23 @@ from google import genai
 
 client = genai.Client(api_key='AIzaSyClaR5XlKEjT6KUComKEK8MjAMOaKjso1g')
 chat = client.chats.create(model="gemini-2.5-flash")
-
+print('''
+1- Einstein
+2- Marie Curie
+3- Alexandre, O grande
+4- Rosa Parks
+5- Alexander Fleming
+                ''')
+persona = input("Digite (entre os listados acima) o qual deseja conversar: ")
 while True:
-    pergunta = input("Digite a pergunta que você quer fazer para Einstein. (Digite 'sair' se quiser parar de fazer perguntas): ")
+    pergunta = input(f"Digite a pergunta que você quer fazer para {persona}. (Digite 'sair' se quiser parar de fazer perguntas): ")
 
     if pergunta.lower() == 'sair':
         break 
 
-    context_response = chat.send_message("Finja que você é o einstein e responda as perguntas como se fosse ele. E responda de forma curta e objetiva")
+    context_response = chat.send_message(f"Finja que você é o {persona} e responda as perguntas como se fosse ele. E responda como se estivesse em uma conversa com uma pessoas porém de forma curta e objetiva")
     response = chat.send_message(pergunta)
-        
-        
 
     print(response.text)
     
-
 print("Conversa encerrada. Até logo!")
